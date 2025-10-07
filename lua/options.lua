211efcd1536c9bd1a6f9c4e8ca7e82323108773f
nanameki1213 <nanamekitaishin1213@icloud.com>
2025-10-07 14:02:35 +0900

    フォーマット

--- lua/options.lua
+++ lua/options.lua
@@ -1,41 +1,41 @@
 -- reference: https://zenn.dev/hisasann/articles/neovim-settings-to-lua
 local options = {
-	encoding = "utf-8",
-	fileencoding = "utf-8",
+	encoding = 'utf-8',
+	fileencoding = 'utf-8',
 	title = true, -- なぞ
-	clipboard = "unnamedplus", -- ヤンクをクリップボードに保存
+	clipboard = 'unnamedplus', -- ヤンクをクリップボードに保存
 	cmdheight = 0, -- コマンドライン領域を非表示にする
 	-- completeopt = { "menuone", "noselect" }, -- vimの標準の補完機能の設定
 	-- conceallevel = 0, -- わからん
 	-- 検索に関する設定
 	hlsearch = true, -- 検索時のハイライトを有効にする
 	ignorecase = true, -- 検索時に大文字/小文字を区別しない
-    mouse = "a", -- マウス操作を有効化
-    -- pumheight = 10, -- 補完メニュー(?)の高さ
-    showmode = true, -- 現在のモードを最下部に表示する
-    showtabline = 2,
-    smartcase = true,
-    smartindent = true,
-    -- swapfile = false, -- スワップファイルを作成しないようにする
-    -- timeoutlen = 300,
-    -- updatetime = 300,
-    -- writebackup = false,
-    -- backupskip = { "/tmp/*", "/provate/tmp" },
-    expandtab = true,
-    shiftwidth = 2,
-    tabstop = 2,
-    cursorline = true,
-    number = true,
-    -- relativenumber = true, -- 行表示を相対的にする
-    numberwidth = 4,
-    signcolumn = "yes",
-    wrap = false,
-    -- 背景透過
-    termguicolors = false, 
-    winblend = 30,
-    pumblend = 30,
-    undodir = "/home/nanameki/.config/nvim/undo",
-    undofile = true,
+	mouse = 'a', -- マウス操作を有効化
+	-- pumheight = 10, -- 補完メニュー(?)の高さ
+	showmode = true, -- 現在のモードを最下部に表示する
+	showtabline = 2,
+	smartcase = true,
+	smartindent = true,
+	-- swapfile = false, -- スワップファイルを作成しないようにする
+	-- timeoutlen = 300,
+	-- updatetime = 300,
+	-- writebackup = false,
+	-- backupskip = { "/tmp/*", "/provate/tmp" },
+	expandtab = true,
+	shiftwidth = 2,
+	tabstop = 2,
+	cursorline = true,
+	number = true,
+	-- relativenumber = true, -- 行表示を相対的にする
+	numberwidth = 4,
+	signcolumn = 'yes',
+	wrap = false,
+	-- 背景透過
+	termguicolors = false,
+	winblend = 30,
+	pumblend = 30,
+	undodir = '/home/nanameki/.config/nvim/undo',
+	undofile = true,
 
 	-- background = "dark",
 	-- scrolloff = 8,
@@ -45,32 +45,32 @@ local options = {
 	-- splitright = false,
 }
 
-vim.opt.shortmess:append("c")
+vim.opt.shortmess:append('c')
 
-if vim.fn.has("wsl") == 1 then
-    if vim.fn.executable("win32yank") == 0 then
-        print("win32yank not found, clipboard integration won't work")
-    else
-        vim.g.clipboard = {
-          name = "win32yank",
-          copy = {
-            ["+"] = 'win32yank -i --crlf',
-            ["*"] = 'win32yank -i --crlf',
-          },
-          paste = {
-            ['+'] = 'win32yank -o --lf',
-            ['*'] = 'win32yank -o --lf',
-          },
-          cache_enabled = true
-        }
-    end
+if vim.fn.has('wsl') == 1 then
+	if vim.fn.executable('win32yank') == 0 then
+		print("win32yank not found, clipboard integration won't work")
+	else
+		vim.g.clipboard = {
+			name = 'win32yank',
+			copy = {
+				['+'] = 'win32yank -i --crlf',
+				['*'] = 'win32yank -i --crlf',
+			},
+			paste = {
+				['+'] = 'win32yank -o --lf',
+				['*'] = 'win32yank -o --lf',
+			},
+			cache_enabled = true,
+		}
+	end
 end
 
 for k, v in pairs(options) do
 	vim.opt[k] = v
 end
 
-vim.cmd("set whichwrap+=<,>,[,],h,l")
-vim.cmd("set modifiable")
+vim.cmd('set whichwrap+=<,>,[,],h,l')
+vim.cmd('set modifiable')
 vim.cmd([[set iskeyword+=-]])
 vim.cmd([[set formatoptions-=cro]])
--- lua/plugins.lua
+++ lua/plugins.lua
@@ -1,4 +1,4 @@
-local lazypath = vim.fn.stdpath('data') ..'/lazy/lazy.nvim'
+local lazypath = vim.fn.stdpath('data') .. '/lazy/lazy.nvim'
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
 	vim.fn.system({
 		'git',
@@ -13,136 +13,136 @@ end
 vim.opt.rtp:prepend(lazypath)
 
 require('lazy').setup({
-  -- Rustのコード整形
-  'rust-lang/rust.vim',
-  -- LSP
-  'mason-org/mason.nvim',
-  'mason-org/mason-lspconfig.nvim',
-  'neovim/nvim-lspconfig',
-  'hrsh7th/nvim-cmp',
-  'hrsh7th/cmp-nvim-lsp',
-  'hrsh7th/vim-vsnip',
-  'hrsh7th/cmp-buffer',
-  'hrsh7th/cmp-path',
-  'onsails/lspkind.nvim',
-  {
-    'windwp/nvim-autopairs',
-    event = "InsertEnter",
-    config = true
-    -- use opts = {} for passing setup options
-    -- this is equivalent to setup({}) function
-  },
+	-- Rustのコード整形
+	'rust-lang/rust.vim',
+	-- LSP
+	'mason-org/mason.nvim',
+	'mason-org/mason-lspconfig.nvim',
+	'neovim/nvim-lspconfig',
+	'hrsh7th/nvim-cmp',
+	'hrsh7th/cmp-nvim-lsp',
+	'hrsh7th/vim-vsnip',
+	'hrsh7th/cmp-buffer',
+	'hrsh7th/cmp-path',
+	'onsails/lspkind.nvim',
+	{
+		'windwp/nvim-autopairs',
+		event = 'InsertEnter',
+		config = true,
+		-- use opts = {} for passing setup options
+		-- this is equivalent to setup({}) function
+	},
 
-  -- GitHub Copilot
-  {
-    'zbirenbaum/copilot.lua',
-    config = function ()
-      require('copilot').setup({
-        suggestion = { enabled = false },
-        panel = { enabled = false },
-      })
-    end
-  },
-  {
-    'zbirenbaum/copilot-cmp',
-    config = function ()
-      require('copilot_cmp').setup()
-    end
-  },
+	-- GitHub Copilot
+	{
+		'zbirenbaum/copilot.lua',
+		config = function()
+			require('copilot').setup({
+				suggestion = { enabled = false },
+				panel = { enabled = false },
+			})
+		end,
+	},
+	{
+		'zbirenbaum/copilot-cmp',
+		config = function()
+			require('copilot_cmp').setup()
+		end,
+	},
 
-  -- みため
-  {
-    'nvim-lualine/lualine.nvim',
-    dependencies = { 'nvim-tree/nvim-web-devicons' }
-  },
-  {
-    'nvim-neo-tree/neo-tree.nvim',
-    branch = 'v3.x',
-    dependencies = {
-      'nvim-lua/plenary.nvim',
-      'nvim-tree/nvim-web-devicons', -- not strictly required, but recommended
-      'MunifTanjim/nui.nvim',
-      -- '3rd/image.nvim', -- Optional image support in preview window: See `# Preview Mode` for more information
-    }
-  },
-  {
-    'shellRaining/hlchunk.nvim',
-    event = { 'BufReadPre', 'BufNewFile' },
-    config = function()
-      require('hlchunk').setup({})
-    end
-  },
-  { "catppuccin/nvim", name = "catppuccin", priority = 1000 }
+	-- みため
+	{
+		'nvim-lualine/lualine.nvim',
+		dependencies = { 'nvim-tree/nvim-web-devicons' },
+	},
+	{
+		'nvim-neo-tree/neo-tree.nvim',
+		branch = 'v3.x',
+		dependencies = {
+			'nvim-lua/plenary.nvim',
+			'nvim-tree/nvim-web-devicons', -- not strictly required, but recommended
+			'MunifTanjim/nui.nvim',
+			-- '3rd/image.nvim', -- Optional image support in preview window: See `# Preview Mode` for more information
+		},
+	},
+	{
+		'shellRaining/hlchunk.nvim',
+		event = { 'BufReadPre', 'BufNewFile' },
+		config = function()
+			require('hlchunk').setup({})
+		end,
+	},
+	{ 'catppuccin/nvim', name = 'catppuccin', priority = 1000 },
 })
 
-require'lualine'.setup {
-  options = {
-    theme = 'auto',
-  },
-  sections = {
-      lualine_a = {'mode'},
-      lualine_b = {'branch', 'diff', 'diagnostics'},
-      lualine_c = {'filename'},
-      lualine_x = {
-        'cdata',
-        'ctime',
-      },
-      lualine_y = {'progress'},
-      lualine_z = {'location'},
-  },
-  inactive_sections = {
-      lualine_a = {},
-      lualine_b = {},
-      lualine_c = {'filename'},
-      lualine_x = {'location'},
-      lualine_y = {},
-      lualine_z = {},
-  },
-  tabline = {},
-  winbar = {},
-  inactive_winbar = {},
-  extensions = {}
-}
+require('lualine').setup({
+	options = {
+		theme = 'auto',
+	},
+	sections = {
+		lualine_a = { 'mode' },
+		lualine_b = { 'branch', 'diff', 'diagnostics' },
+		lualine_c = { 'filename' },
+		lualine_x = {
+			'cdata',
+			'ctime',
+		},
+		lualine_y = { 'progress' },
+		lualine_z = { 'location' },
+	},
+	inactive_sections = {
+		lualine_a = {},
+		lualine_b = {},
+		lualine_c = { 'filename' },
+		lualine_x = { 'location' },
+		lualine_y = {},
+		lualine_z = {},
+	},
+	tabline = {},
+	winbar = {},
+	inactive_winbar = {},
+	extensions = {},
+})
 
 require('catppuccin').setup()
 
 require('hlchunk').setup({
-  chunk = {
-    enable = true,
-  },
-  indent = {
-    enable = true,
-  },
+	chunk = {
+		enable = true,
+	},
+	indent = {
+		enable = true,
+	},
 })
 
 require('neo-tree').setup({
-  filesystem = {
-    window = {
-      mappings = {
-        ["R"] = function(state)
-          local node = state.tree.get_node()
-          if node.type == "directory" then
-            require('neo-tree.sources.filesystem').navigate(state, node:get_id())
-          end
-        end,
-      },
-    },
-  },
+	filesystem = {
+		window = {
+			mappings = {
+				['R'] = function(state)
+					local node = state.tree.get_node()
+					if node.type == 'directory' then
+						require('neo-tree.sources.filesystem').navigate(state, node:get_id())
+					end
+				end,
+			},
+		},
+	},
 })
 
 -- Reference: https://zenn.dev/botamotch/articles/21073d78bc68bf
 -- 1. LSP Server management
 require('mason').setup()
-require("mason-lspconfig").setup({
-  ensure_installed = { "rust_analyzer", "tsserver", "lua_ls" },
-});
+require('mason-lspconfig').setup({
+	ensure_installed = { 'rust_analyzer', 'tsserver', 'lua_ls' },
+})
 
 -- 2. build-in LSP function
 
 local keymap = vim.keymap
 
 -- keyboard shortcut
-keymap.set('n', 'K',  '<cmd>lua vim.lsp.buf.hover()<CR>')
+keymap.set('n', 'K', '<cmd>lua vim.lsp.buf.hover()<CR>')
 keymap.set('n', 'gf', '<cmd>lua vim.lsp.buf.formatting()<CR>')
 keymap.set('n', 'gr', '<cmd>lua vim.lsp.buf.references()<CR>')
 keymap.set('n', '<F12>', '<cmd>lua vim.lsp.buf.definition()<CR>')
@@ -170,25 +170,25 @@ keymap.set('n', 'sl', '<C-w>l')
 -- 3. completion (hrsh7th/nvim-cmp)
 local cmp = require('cmp')
 cmp.setup({
-  snippet = {
-    expand = function(args)
-      vim.fn['vsnip#anonymous'](args.body)
-    end,
-  },
-  sources = {
-    { name = 'copilot' },
-    { name = 'nvim_lsp' },
-    { name = 'buffer' },
-    { name = 'path' },
-  },
-  mapping = cmp.mapping.preset.insert({
-    ['<C-p>'] = cmp.mapping.select_prev_item(),
-    ['<C-n>'] = cmp.mapping.select_next_item(),
-    ['<C-l>'] = cmp.mapping.complete(),
-    ['<C-e>'] = cmp.mapping.abort(),
-    ['<Tab>'] = cmp.mapping.confirm { select = true },
-  }),
-  experimental = {
-    ghost_text = true,
-  },
+	snippet = {
+		expand = function(args)
+			vim.fn['vsnip#anonymous'](args.body)
+		end,
+	},
+	sources = {
+		{ name = 'copilot' },
+		{ name = 'nvim_lsp' },
+		{ name = 'buffer' },
+		{ name = 'path' },
+	},
+	mapping = cmp.mapping.preset.insert({
+		['<C-p>'] = cmp.mapping.select_prev_item(),
+		['<C-n>'] = cmp.mapping.select_next_item(),
+		['<C-l>'] = cmp.mapping.complete(),
+		['<C-e>'] = cmp.mapping.abort(),
+		['<Tab>'] = cmp.mapping.confirm({ select = true }),
+	}),
+	experimental = {
+		ghost_text = true,
+	},
 })
